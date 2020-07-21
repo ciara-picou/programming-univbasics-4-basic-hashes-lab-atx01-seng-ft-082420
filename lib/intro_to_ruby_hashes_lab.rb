4c88da66e4b75bd95d772e5ad6016426799908ef
Ciara Picou <ciarapicou@att.net>
2020-07-21 20:13:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,11 @@ end
 
 def my_hash
  favourite_paintings = {
-   :name : ""
+   name : "The Blue Room" ,
+   artist : "Suzanne Valadon" ,
+   movement : "Post Impressionism"
+   year : 
+   
  }
 end
 
